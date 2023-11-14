80108e76a86b69e5326a5522e8cb91c2b6d82a5f
FlavioRDS <frdsilva@yahoo.com.br>
2023-11-14 11:02:15 -0300

    Fazendo o SpawnPoint para o Mude cena

---
+++ Assets/RectangleKnight_Scripts/controladores/StaticMudeCenaComSpawnPoint.cs
@@ -0,0 +1,93 @@
+﻿using Spawns;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class StaticMudeCenaComSpawnPoint
+{
+    private static int contCenasCaregadas = 0;
+    private static int numCenasParaCarregar = 0;
+    private static NomesCenas cenaAtiva = NomesCenas.nula;
+    private static SpawnID spawnId;
+
+    public static bool EstaCenaEstaCarregada(NomesCenas nome)
+    {
+        return SceneManager.GetSceneByName(nome.ToString()).isLoaded;
+    }
+
+    public static void OnFadeOutComplete(NomesCenas[] cenasAlvo, NomesCenas estaCenaAtiva, SpawnID pos)
+    {
+
+
+        spawnId = pos;
+
+        if (estaCenaAtiva != NomesCenas.nula)
+            cenaAtiva = estaCenaAtiva;
+        else
+            cenaAtiva = cenasAlvo[0];
+
+        Time.timeScale = 0;
+        contCenasCaregadas = 0;
+
+        NomesCenas[] N = SceneLoader.DescarregarCenasDesnecessarias(cenasAlvo);
+
+        for (int i = 0; i < N.Length; i++)
+        {
+            SceneManager.UnloadSceneAsync(N[i].ToString());
+        }
+
+        N = SceneLoader.PegueAsCenasPorCarregar(cenasAlvo);
+
+        numCenasParaCarregar = N.Length;
+
+        for (int i = 0; i < N.Length; i++)
+        {
+            SceneManager.LoadScene(N[i].ToString(), LoadSceneMode.Additive);
+        }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private static void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        contCenasCaregadas++;
+
+        if (contCenasCaregadas >= numCenasParaCarregar)
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+
+            SceneManager.activeSceneChanged += OnActiveSceneChanged;
+
+
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(cenaAtiva.ToString()));
+
+
+        }
+    }
+
+    private static void OnActiveSceneChanged(Scene arg0, Scene arg1)
+    {
+        SceneManager.activeSceneChanged -= OnActiveSceneChanged;
+        Time.timeScale = 1;
+        var v = MonoBehaviour.FindObjectsOfType<SpawnPointMark>();
+        Vector3 posAlvo=Vector3.zero;
+        foreach (var w in v)
+            if (w.IdSpawn == spawnId)
+                posAlvo = w.transform.position;
+
+        GameController.g.Manager.transform.position = posAlvo;
+        GameController.g.VerifiqueDinheiroCaido(GameController.g.Manager.Dados.DinheiroCaido);
+        GlobalController.g.FadeV.IniciarFadeInComAction(OnFadeInComplete);
+        MonoBehaviour.FindObjectOfType<Camera2D>().AposMudarDeCena(posAlvo + new Vector3(0, 0, -10));
+        EventAgregator.Publish(EventKey.changeActiveScene, null);
+
+        new MyInvokeMethod().InvokeAoFimDoQuadro(() =>
+        {
+            EventAgregator.Publish(EventKey.localNameExibition);
+        });
+    }
+
+    static void OnFadeInComplete()
+    {
+
+    }
+}
---
+++ Assets/RectangleKnight_Scripts/controladores/mudeCenaExterno/SpawnPointMark.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spawns
+{
+    public class SpawnPointMark : MonoBehaviour
+    {
+        [SerializeField] private SpawnID idSpawn;
+
+        public SpawnID IdSpawn { get => idSpawn; }
+
+        public static SpawnPointMark GetSpawnPointById(SpawnID id)
+        {
+
+            SpawnPointMark[] spawns = FindObjectsOfType<SpawnPointMark>();
+
+            for (int i = 0; i < spawns.Length; i++)
+            {
+                if (id == spawns[i].IdSpawn)
+                    return spawns[i];
+            }
+
+            return null;
+            
+        }
+    }
+
+    public enum SpawnID
+    { 
+        numero1,
+        numero2,
+        numero3,
+        numero4,
+        numero5,
+        numero6,
+        numero7,
+        numero8,
+        numero9,
+        numero10,
+        numero11,
+        numero12,
+
+    }
+}
