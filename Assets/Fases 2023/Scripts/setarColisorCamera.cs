507bee0294dc1c6f06dd943d1e656027ba3a4bf7
Gape-42 <gabrielcarmempereira@gmail.com>
2023-12-11 15:16:27 -0300

    teste camera

---
+++ Assets/Fases 2023/Scripts/setarColisorCamera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+
+public class setarColisorCamera : MonoBehaviour
+{
+    public CinemachineConfiner confiner;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.activeSceneChanged += onChangeActiveScene;
+
+        onChangeActiveScene(default, default);
+    }
+
+    private void onChangeActiveScene(Scene arg0, Scene arg1)
+    {
+        
+
+        var v =  GameObject.FindGameObjectWithTag("ColisorCamera");
+
+        SceneManager.MoveGameObjectToScene(v, SceneManager.GetSceneByName("ComunsDeFase"));
+
+        confiner.m_BoundingShape2D = v.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
